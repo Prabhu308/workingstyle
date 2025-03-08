f85834a6ec943b1a411b426358bee0521d0cedcd
Prabhu <prabu502@gmail.com>
2025-03-08 10:22:44 +0530

    Add files via upload

---
+++ work.java
@@ -0,0 +1,14 @@
+package accessing;
+
+public class work {
+
+	public work() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("work.java");
+	}
+
+}
